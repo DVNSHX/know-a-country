8f809df2cc29071eabe153d714603a1db609ea28
devanshdev <tyagidevanshofficial@gmail.com>
2019-10-10 18:42:57 +0530

    Fixed searchbox and button

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -4,8 +4,8 @@ import './SearchBox.css'
 const SearchBox = (props) =>{
 	return(
 		<div className="searchbox">
-			<input type="text" className="inputfield" placeholder="Input a Country Name"/>
-			<button className="btn first" onClick={props.onSearch}>Search</button>
+			<input type="text" className="inputfield" onChange={props.onSearch} placeholder="Input a Country Name"/>
+			<button className="btn first" onClick={props.onClick}>Search</button>
 		</div>
 		)
 }
